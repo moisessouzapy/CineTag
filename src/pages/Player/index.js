9360592cc72f81363493c72e4ba6c4899d49170a
moises <moisessouza0204@gmail.com>
2023-01-11 12:53:58 -0300

    first commit

---
+++ src/pages/Player/index.js
@@ -0,0 +1,48 @@
+import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+import { useParams } from "react-router-dom";
+import styles from "./Player.module.css";
+import NaoEncontrada from "pages/NaoEncontrada";
+import { useEffect, useState } from "react";
+
+function Player() {
+  const [video, setVideos] = useState();
+  const parametros = useParams();
+
+  useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/moisessouzapy/api/videos?id=${parametros.id}`
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(...dados);
+      });
+  }, []);
+
+  if (!video) {
+    return <NaoEncontrada></NaoEncontrada>;
+  }
+
+  return (
+    <>
+      <Banner imagem="player"></Banner>
+      <Titulo>
+        <h1>Player</h1>'
+        <section className={styles.container}>
+          <iframe
+            width="100%"
+            height="100%"
+            src={video.link}
+            title={video.titulo}
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </section>
+        '
+      </Titulo>
+    </>
+  );
+}
+
+export default Player;
